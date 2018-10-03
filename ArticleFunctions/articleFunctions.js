b8612321fa82bdffe90bcfd2d3fe337f5e16ec37
Evelyn Jepsen <evelynjepsen@gmail.com>
2018-10-03 11:08:09 +0200

    things work

--- ArticleFunctions/articleFunctions.js
+++ ArticleFunctions/articleFunctions.js
@@ -1,11 +1,22 @@
 let json = require("../articles");
+import bodyParser from 'body-parser';
+let art = require("../models/articleModels");
+let mongoose = require('mongoose');
+let express = require("express");
+// import {articleSchema} from '../models/articleModels'
+// import {article} from '../models/articleModels'
+
+
+
+
 
 class ArticleFunctions {
 
+
   getAllArticles(req, res) {
     let articleArray = [];
-    let limit = req.query.limit;
-    let skip = req.query.skip;
+    let limit = Number(req.query.limit);
+    let skip = Number(req.query.skip);
     for(let i=skip; i<=limit; i++ ){
       json.map(article => {
         if(i== article.id) {
@@ -13,11 +24,16 @@ class ArticleFunctions {
         }
       })
     }
-    if (skip>0) {
+    if (skip>0&&limit>0&&skip<=limit) {
       return res.status(200).send({
         success: 'true',
         message: 'Articles retrieved successfully',
         articles: articleArray
+      })
+    } else if (skip>limit) {
+      return res.status(404).send({
+        success: 'false',
+        message: 'Requested amout of articles higher than requested max amount'
       })
     } else {
       return res.status(200).send({
@@ -27,6 +43,44 @@ class ArticleFunctions {
       })
     }
   }
+
+  postArticle(req, res) {
+    if(!req.body.product_name) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'A product name is required'
+      });
+    } else if(!req.body.price) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'Price is required'
+      });
+    }else if(!req.body.Category) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'Category is required'
+      });
+    }
+    let article = new art ({
+      _id: mongoose.Types.ObjectId(),
+      id: json.length+1,
+      product_name: req.body.product_name,
+      price: req.body.price,
+      Category: req.body.Category
+    })
+    json.push(article);
+    article.save()
+    .then(item => {
+      res.send({
+        success: 'true',
+        message: 'Article added successfully',
+        article
+      });
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
+  };
 
   getSingleArticle(req, res) {
     const id = req.params.id;
@@ -47,6 +101,8 @@ class ArticleFunctions {
     })
   }
 }
+
+
 
 
 const articleFunctions = new ArticleFunctions();
--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+let express = require("express");
+let models = require("./models/articleModels");
+import bodyParser from 'body-parser';
+import articleSchema from './models/articleModels'
 import router from './routes/index.js';
 
-let express = require("express");
+
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
+
 
 const port = 5000;
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`)
 })
+
+/*
+if ('development' == app.get('env')) {
+app.use(express.errorhandler());
+mongoose.connect('mongodb://55.55.55.5/mongo')
+}
+mongoose.model('users', {name: String})
+*/
